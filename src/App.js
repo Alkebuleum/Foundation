a44cd252e690b9c2a119c1db990c72a9c5121fb9
Dwehsidi <42282793+Dwehsidi@users.noreply.github.com>
2024-09-26 11:57:19 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './logo.PNG'; // or logo.svg
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="Alkebuleum Foundation Logo" />
+        <div className="container">
+          <h1 className="title">Alkebuleum Foundation</h1>
+          <p className="slogan">We support and grow the Alkebuleum Ecosystem</p>
+          <div className="summary">
+            <p>
+              The Foundation’s aim is to realize more inclusive, fair, and just institutions
+              of governance and of the global digital economy. To that end, it serves as the
+              steward of the Alkebuleum protocol, supporting and growing the ecosystem until it
+              becomes self-sufficient.
+            </p>
+          </div>
+          {/* External link */}
+          <a href="https://www.alkebuleum.org" target="_blank" rel="noopener noreferrer" className="learn-more-button">
+            Learn More
+          </a>
+        </div>
       </header>
     </div>
   );
